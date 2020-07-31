1b7b64b767e4852fef8f75f6af32f86117fb67e8
iansanwich <iansanwich@gmail.com>
2020-07-31 18:12:51 +0800

    Add content

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+console.log("TEST")
+
+const myDate = new Date();
+const hrs = myDate.getHours();
+
+let greet = "Hi there!";
+
+if (hrs < 12) greet = "Good morning!";
+else if (hrs >= 12 && hrs <= 17) greet = "Good afternoon!";
+else if (hrs >= 17 && hrs <= 24) greet = "Good evening!";
+
+document.getElementById("greetingsText").innerHTML = greet;
